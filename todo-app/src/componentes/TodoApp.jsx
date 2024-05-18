fa71e42b6623d619629e1053b911b98f3ed39ad5
Shehan Vinod <110800392+ShehanGit@users.noreply.github.com>
2024-05-18 18:33:41 +0530

    Login  half finished

--- todo-app/src/componentes/TodoApp.jsx
+++ todo-app/src/componentes/TodoApp.jsx
@@ -1,3 +1,6 @@
+import { useState } from 'react'
+import '../CSS/Todo.css'
+
 export default function TodoApp(){
     return(
         <div className="TodoApp">
@@ -9,9 +12,38 @@ export default function TodoApp(){
 }
 
 function LoginComponent(){
+
+    const [username, setUsername] =useState ('in26Minits')
+    const [passward, setPassward] =useState ('')
+
+
+    function handleUsernameChange(event){
+        setUsername(event.target.value)
+    }
+
+    function handlePasswardChange(event){
+        setPassward(event.target.value)
+        console.log(event.target.value)
+    }
+       
+
     return(
         <div  className="Login">
-            <h1>Login Component</h1>
+            <div className="loginForm">
+                <div>
+                    <label>User name :</label>
+                    <input type ="text" name="username" value={username} onChange={handleUsernameChange} />
+                </div>
+
+                <div>
+                    <label>Passward :</label>
+                    <input type ="password" name="passward" value={passward} onChange={handlePasswardChange}/>
+                </div>
+                <div>
+                    <button type="button" name="login"> Login </button>
+                </div>
+            </div>
+            
         </div>
     )
 }
